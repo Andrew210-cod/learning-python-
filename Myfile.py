6293bf0563e33ea20ce20d6d095b49b4787c3ae9
Andrew210-cod <andrewalavarez@gmail.com>
2025-07-17 13:21:07 -0400

    Add files via upload

---
+++ Myfile.py
@@ -0,0 +1,26 @@
+
+num1= input("enter the first number: ")
+num1= float(num1)
+
+op= input("Enter an operater(+,-,*,/): ")
+
+num2= input("Enter the second value: ")
+num2= float(num2)
+
+if op=="+":
+ result= num1 + num2
+
+elif op=="-":
+ result= num1 - num2
+
+elif op=="*":
+ result= num1 * num2
+
+else: 
+ result= num1 / num2 
+
+print(result)
+
+
+
+
