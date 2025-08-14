8053a1e3a4b3e83256aa9328aa51edad543f6440
Andrew210-cod <andrewalavarez@gmail.com>
2025-08-14 13:09:51 -0400

    Add files via upload

---
+++ Python_Function.py
@@ -0,0 +1,30 @@
+def add( a, b):
+    return a+b
+
+print(add(5,5))
+
+def sub( a, b):
+    return a-b
+
+print(sub(10,7))
+
+def mul( a, b):
+    return a*b
+
+print(mul(5,6))
+
+def div( a, b):
+    return a//b
+
+print(div(8,4))
+
+print(mul(10,90))
+
+Apple= add(5,6)
+
+print(Apple) 
+
+def add( a, b, c):
+    return a+b+c
+
+print(add(3,4,5))
